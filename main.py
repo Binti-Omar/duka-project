af8a2dc852778b4e2f1d07df87abf668c243037e
unknown <chombobintiomar5gmail.com>
2025-10-28 16:53:59 +0300

    add products

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-from database import fetch_data
+from flask import Flask,render_template,request,redirect,url_for
+from database import fetch_data,insert_product
 
 app=Flask(__name__)
 
@@ -12,10 +12,27 @@ def products():
     prods=fetch_data('products')
     return render_template ('products.html',prods=prods)
 
+
+@app.route('/add_products',methods=['GET','POST'])
+def add_products():
+    if request.method=='POST':
+        # getting form input to serverside
+        product_name=request.form['name']
+        buying_price=request.form['bp']
+        selling_price=request.form['sp']
+        # print(product_name,selling_price,buying_price)
+        new_product=(product_name,buying_price,selling_price)
+        # inserting data to database
+        insert_product(new_product)
+        # redirect is used to direct to a url/certain page and url_for takes a function name then takes you to its route
+    return redirect(url_for('products'))
+
 @app.route('/sales')
 def sales():
     sales=fetch_data('sales')
     return render_template ('sales.html',sales=sales)
+
+
 
 @app.route('/stock')
 def stocks():
